8b5d7ebea6642d9c93d1963617530ced872aeb13
eric <ericjdeveloper@gmail.com>
2023-12-07 20:28:33 -0500

    fixed tabular qlearning to use new env

--- main.py
+++ main.py
@@ -92,24 +92,11 @@ def linear_regression():
     env.close()
 
 def run():
-    env = FlappyEnv()
+    env = FlappyEnv2()
 
-    # model = qlearning.QLearner()
-    # Pi, Q = await model.run(env, gamma=0.9, step_size=0.1, epsilon=0.1, max_episode=60000, callback_step=100, callback=callback)
-    # Pi.tofile('output/Pi.csv', sep=",")
-
-    for episode in range(10):
-        env.reset()
-        total_reward = 0
-        while True:
-            action = env.action_space.sample()
-            action = 0 if env.np_random.choice(20) == 0 else 0
-            obs, reward, game_over, _, info = env.step(action)
-            total_reward += reward
-            # print(obs)
-            if game_over:
-                callback(episode+1, total_reward)
-                break           
+    model = qlearning.QLearner()
+    Pi, Q = model.run(env, gamma=0.9, step_size=0.1, epsilon=0.1, max_episode=60000, callback_step=100, callback=callback)
+    Pi.tofile('output/Pi.csv', sep=",")
     env.close()
 
 def callback(episode:int=None, score:float=None, Q=None):
@@ -137,4 +124,4 @@ def run_ac():
     print(eval_returns)
 
 if __name__ == "__main__":
-    run_ac()
+    run()
